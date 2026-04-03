6ab93521d5fdb4736b1d62ce676de441dcbe80f0
Anupam Manoj <anupammanoj1157@gmail.com>
2026-04-03 11:32:10 +0530

    Completed Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,12 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String[] args) {
+        int height = 3;
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 0; j < (2 * i - 1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
